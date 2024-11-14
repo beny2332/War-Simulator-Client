4ebe60cc081c592506d13ccb3c0a040248c9bbfe
BinyaminYacovson <bj57577@gmail.com>
2024-11-14 16:10:22 +0200

    rename Status enum to attackStatus and add DefenseOrganizations and AttackOrganizations enums

--- src/types/enums/attackStatus.ts
+++ src/types/enums/attackStatus.ts
@@ -1,4 +1,4 @@
-export enum Status{
+export enum attackStatus{
     Launched = "Launched",
     Intercepted = "Intercepted",
     Hit = "Hit"
---
+++ src/types/enums/organizations.ts
@@ -0,0 +1,13 @@
+export enum DefenseOrganizations {
+    IDF_North = "IDF - North",
+    IDF_South = "IDF - South",
+    IDF_Center = "IDF - Center",
+    IDF_WestBank = "IDF - West Bank",
+  }
+  
+  export enum AttackOrganizations {
+    Hezbollah = "Hezbollah",
+    Hamas = "Hamas",
+    Houthis = "Houthis",
+    IRGC = "IRGC",
+  }
