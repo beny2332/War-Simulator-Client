4b03f1075f2bb363cd66c5503515ea20b3067946
BinyaminYacovson <bj57577@gmail.com>
2024-11-14 09:13:31 +0200

    implement routing and redux store setup with protected routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
+import { io } from 'socket.io-client'
+
+export const socket = io("http://localhost:1234")
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
 )
